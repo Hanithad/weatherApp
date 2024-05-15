47b6af4808907e32174339f7cf8c47d58f405344
Hanithad <dhanitha92@gmail.com>
2024-05-15 15:35:32 -0500

    created a collection view and having a scroll bar(Horizontal) for hourly update of weather, and we in weatherDetail added HourlyWeatherdata

---
+++ weatherApp/HourlyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HorlyCollectionViewCell.swift
+//  weatherApp
+//
+//  Created by Hanitha Dhavileswarapu on 5/14/24.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    
+}
